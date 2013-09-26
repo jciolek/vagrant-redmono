c35a650762cc25f527195ef736c35600060c651b
Jacek Ciolek <jacek@webnicer.com>
2013-09-26 01:17:16 +0100

    get rid of stages and make the package classes more self-contained

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -2,13 +2,7 @@ Exec {
   path => '/usr/bin:/bin:/usr/local/bin',
 }
 
-stage { 'bootstrap':
-  before => Stage['main'],
-}
-
-class add_repos {
-  class { 'apt': }
-
+class ppa {
   exec { 'apt-get update':
     command => 'apt-get update',
   }
@@ -17,28 +11,17 @@ class add_repos {
     ensure => installed,
     require => Exec['apt-get update'],
   }
-
-  apt::ppa { 'ppa:chris-lea/node.js':
-    require => Package['python-software-properties'],
-  }
-
-  apt::ppa { 'ppa:chris-lea/redis-server':
-    require => Package['python-software-properties'],
-  }
-
-  apt::source { 'mongodb':
-    location => 'http://downloads-distro.mongodb.org/repo/ubuntu-upstart',
-    release => 'dist',
-    repos => '10gen',
-    key => '7F0CEB10',
-    key_server => 'keyserver.ubuntu.com',
-    include_src => false,
-  }
 }
 
 class nodejs {
+  apt::ppa { 'nodejs':
+    name => 'ppa:chris-lea/node.js',
+    require => Class['ppa'],
+  }
+
   package { 'nodejs':
     ensure => latest,
+    require => Apt::Ppa['nodejs'],
   }
 }
 
@@ -60,21 +43,56 @@ class nodejs_modules {
 }
 
 class redis {
+  apt::ppa { 'redis-server':
+    name => 'ppa:chris-lea/redis-server',
+    require => Class['ppa'],
+  }
+
   package { 'redis-server':
     ensure => latest,
+    require => Apt::Ppa['redis-server'],
+  }
+
+
+  file { '/etc/redis':
+    ensure => directory,
+    owner => root,
+    group => root,
+    before => File['redis.conf'],
+  }
+
+  file { 'redis.conf':
+    path => '/etc/redis/redis.conf',
+    source => 'puppet:///modules/redis/redis.conf',
+    owner => root,
+    group => root,
+    before => Package['redis-server'],
+  }
+
+  service { 'redis-server':
+    require => Package['redis-server'],
+    ensure => running,
   }
 }
 
 class mongodb {
+  apt::source { 'mongodb':
+    location => 'http://downloads-distro.mongodb.org/repo/ubuntu-upstart',
+    release => 'dist',
+    repos => '10gen',
+    key => '7F0CEB10',
+    key_server => 'keyserver.ubuntu.com',
+    include_src => false,
+  }
+  
   package { 'mongodb-10gen':
     ensure => installed,
+    require => Apt::Source['mongodb'],
   }
 }
 
-class { 'add_repos':
-  stage => bootstrap,
-}
-
+class { 'apt': }
+class { 'ppa': }
 class { 'nodejs': }
 class { 'redis': }
 class { 'mongodb': }
